b7c66eecd5654ae941d7457c0d7875f7cf96889f
Abdulla Faraz <abdulla.faraz@gmail.com>
2025-03-08 17:22:54 +0530

    adding shadcn elements

--- services/web/src/components/StatelessApp/index.tsx
+++ services/web/src/components/StatelessApp/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 // Default styles that can be overridden by your app
 
 import Bar from '../Bar';
-import {useWalletMethods} from '../../hooks/useWalletMethods.ts';
+import {useWalletMethods} from '../../hooks/use-wallet-methods.ts';
 
 export const StatelessApp = (): React.ReactElement => {
 
